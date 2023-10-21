6c5d81c4b11c3913b0ee99348bf3e5092b23c105
Sakif-Al-Faruque <sakif.saf915@gmail.com>
2023-10-21 08:14:17 +0600

    frontend is in dev 1 - boostrap & custom style are added

---
+++ resources/views/home.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blog App - Home</title>
+
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Anton&family=Montserrat:wght@100;200;300;400;500;600;800&family=Nunito:wght@200;300;400;500;600;700;800;900&family=Open+Sans:wght@300;400;500;600;700;800&family=Oswald:wght@200;300;600;700&family=Raleway:wght@200;400;500;700;800&family=Roboto+Slab:wght@100;300;400;500;600;700;800;900&family=Roboto:wght@100;400;700;900&display=swap" rel="stylesheet">
+
+
+
+    <link rel="stylesheet" href="{{url('frontend/css/bootstrap.min.css')}}">
+    <link rel="stylesheet" href="{{url('frontend/css/style.css')}}">
+</head>
+<body>
+    
+
+    <h1 class="bg-success">hello</h1>
+    
+    <script src="{{url('frontend/js/bootstrap.bundle.min.js')}}"></script>
+    <script src="{{url('frontend/js/custom.js')}}"></script>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/home', function () {
+    return view('home');
+});
+
 //CRUD Test
 
 //Blog -
