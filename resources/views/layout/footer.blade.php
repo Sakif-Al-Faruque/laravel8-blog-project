454fcf5f7f77f4e639490b70e34c18321fdd55ba
Sakif-Al-Faruque <sakif.saf915@gmail.com>
2023-10-21 19:47:00 +0600

    frontend is in dev 2 - layout is added

---
+++ resources/views/layout/footer.blade.php
@@ -0,0 +1,5 @@
+    <h1 class="bg-danger">footer</h1>
+    <script src="{{url('frontend/js/bootstrap.bundle.min.js')}}"></script>
+    <script src="{{url('frontend/js/custom.js')}}"></script>
+</body>
+</html>
